0ed24bbf2aca26560efce3a6c51e29adf8fc3c3b
rahulkushwah <rahul52us@gmail.com>
2025-03-22 15:05:58 +0530

    change title

--- app/(main)/blogs/components/VideoCarousel/VideoCarousel.tsx
+++ app/(main)/blogs/components/VideoCarousel/VideoCarousel.tsx
@@ -63,7 +63,7 @@ const VideoCarousel = observer(() => {
   return (
     <Box p={{ base: 8, lg: 4 }} maxW={{ lg: "85%", xl: '80%' }} mx={'auto'}>
       <CustomSmallTitle>VIDEO LIBRARY</CustomSmallTitle>
-      <CustomSubHeading highlightText="Understanding Mental Health with Experts"> </CustomSubHeading>
+      <CustomSubHeading highlightText="Epic Travel Moments: Watch & Explore"> </CustomSubHeading>
       <Box mt={6}>
 
         <Slider {...settings}>
