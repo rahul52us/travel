35f871413ffc3df26e09e89efde35a90fa9e1233
rahulkushwah <rahul52us@gmail.com>
2025-07-25 23:34:31 +0530

    hanlde changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme/theme"; // Import the default exported theme
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
 import MainLayout from "./layouts/mainLayout/MainLayout";
 import AuthenticationLayout from "./layouts/authenticationLayout/AuthenticationLayout";
 import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
@@ -9,20 +8,26 @@ import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import stores from "./store/stores";
 import Notification from "./component/common/Notification/Notification";
-import Script from 'next/script';
-import { observer } from "mobx-react-lite";
+import { Montserrat } from 'next/font/google';
+import WhatsAppButton from "./component/common/whatsApp/whatsAppButton";
 
-const RootLayout = observer(({ children }: { children: React.ReactNode }) => {
+const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '500', '600', '700'] });
 
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   const { companyStore: { getCompanyDetails } } = stores;
   const pathname = usePathname();
+  const theme = extendTheme(stores.themeStore);
 
   useEffect(() => {
     getCompanyDetails();
-  }, [getCompanyDetails]);
+  }, [getCompanyDetails]); // This is fine as is
 
   const getLayout = () => {
-    if (pathname === '/login' || pathname === '/register' || pathname === "/forgot-password") {
+    if (pathname === '/login' || pathname === '/register' || pathname === "/forgot-password" || pathname === '/signUp') {
       return AuthenticationLayout;
     } else if (pathname.startsWith('/dashboard')) {
       return DashboardLayout;
@@ -35,54 +40,15 @@ const RootLayout = observer(({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <head>
-        {/* ✅ Corrected Meta Title & Description for SEO */}
-        <title>Cosmic Travels</title>
-        <meta
-          name="description"
-          content="Cosmic Travels"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="UTF-8" />
-
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WQW7482D');
-          `}
-        </Script>
-
-        {/* Meta Pixel Code */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '615471921116956');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-
         <ColorModeScript initialColorMode="light" />
       </head>
-
-      <body>
-
+      <body className={`${montserrat.className}`} style={{ margin: 0, padding: 0 }}>
         <ChakraProvider theme={theme}>
           <Notification />
           <LayoutComponent>{children}</LayoutComponent>
+          <WhatsAppButton />
         </ChakraProvider>
       </body>
     </html>
   );
-})
-
-export default RootLayout
+}
